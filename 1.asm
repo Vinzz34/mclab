a078bbd94fb5a6ecd8e408ca96fcb3e904a60f9d
Vinay G N <76006184+Vinzz34@users.noreply.github.com>
2023-07-24 20:54:18 +0530

    Update 1.asm

--- 1.asm
+++ 1.asm
@@ -1,13 +1,13 @@
 PRESERVE8
-		THUMB
-		AREA |.text|,CODE, READONLY
-	 EXPORT __main
+	THUMB
+	AREA |.text|,CODE, READONLY
+EXPORT __main
 __main
-          LDRS R1,=0x00000200
-         LDRS R2,=0x00000400
-         LDMIA R1!, {R3,R4}
+        LDRS R1,=0x00000200
+        LDRS R2,=0x00000400
+        LDMIA R1!, {R3,R4}
         MOV SP,  R2
-       PUSH(R3,R4)
-       POP(R6,R7)
-STOP B STOP
-        END
+       	PUSH(R3,R4)
+       	POP(R6,R7)
+ STOP B STOP
+END
