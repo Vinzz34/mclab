9d752843f7d716c1e875163b500eb92414b1a437
Vinay G N <76006184+Vinzz34@users.noreply.github.com>
2023-07-24 22:55:06 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -11,19 +11,16 @@ run using "Python3 file_name"
 """
 import RPi.GPIO as GPIO
 import time
-# Set the GPIO mode to BCM
+
 GPIO.setmode(GPIO.BCM)
-# Set up GPIO pin 17 as an output pin
-led_pin = 17
-GPIO.setup(led_pin, GPIO.OUT)
-try:
-  while True:
-    # Turn on the LED
-    GPIO.output(led_pin, GPIO.HIGH)
-    time.sleep(1) # Wait for 1 second
-    # Turn off the LED
-    GPIO.output(led_pin, GPIO.LOW)
-    time.sleep(1) # Wait for 1 second
-except KeyboardInterrupt:
-  # Clean up GPIO configuration on keyboard interrupt (Ctrl+C)
-  GPIO.cleanup() 
+
+GPIO.setup(17, GPIO.OUT)
+GPIO.output(17, GPIO.HIGH)
+
+time.sleep(3)
+
+GPIO.output(17, GPIO.LOW)
+
+time.sleep(3)
+GPIO.cleanup()
+
