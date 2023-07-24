2fc5580f880ecc8cf160624897de34ef6cbb2200
Vinay G N <76006184+Vinzz34@users.noreply.github.com>
2023-07-24 22:50:26 +0530

    Update 4.asm

--- 4.asm
+++ 4.asm
@@ -1,43 +1,45 @@
-;create seperate files and add all to the source group
-i)main.asm
-PRESERVE8
-		THUMB
-		AREA |.text|,CODE, READONLY
-	 EXPORT __main
-	EXTERN func
+;main.asm
+
+	PRESERVE8       
+        	THUMB           
+                 AREA    |.text|, CODE, READONLY
+	EXPORT __main 
+	EXTERN  func
+
 __main
-	LDR R0,=0x10 ;VALUE OF X
-	BL func
-Stop b stop
+	LDR r0,=0x10;
+ 	BL func
+stop B stop
   END
+
+
+;(ii) func.asm
+
+PRESERVE8
+	THUMB
+	AREA |.text|, CODE, READONLY
+	EXPORT func
+	EXTERN func2
 	
-2)func.asm
-PRESERVE8
-		THUMB
-		AREA |.text|,CODE, READONLY
-	 EXPORT func
-	EXTERN func1
 func
-	;calculating value of y
-PUSH{LR}
-;load value of x to r1
-MOVS R1,R0
-;MULTIPLY 2 to  r1
-MULS R1,R1,#2
-; ADD 9 to the x value
-ADDS R1,R1,#9
-BL func1
-POP{PC}
-END
+	push{LR}
+	LDR R3,=0x2
+	MULS R0, R3, R0
+	movs R1, R0
+	BL func2
+	pop{PC}
+    	END
 
-3)func1.asm
 
-PRESERVE8
-		THUMB
-		AREA |.text|,CODE, READONLY
-	 EXPORT func1
+;(iii) func2.asm
 
-func1
-	ADDS R2,R1
+      PRESERVE8
+	THUMB
+	AREA |.text|, CODE, READONLY
+	EXPORT func2
+	
+func2
+	MOVS r2,#09
+	adds R1, R1, R2
 	BX LR
-END
+	   END
