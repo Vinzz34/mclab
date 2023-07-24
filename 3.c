40829e322883a04c3267b54d6c5ccb71480e0f55
Vinay G N <76006184+Vinzz34@users.noreply.github.com>
2023-07-24 21:16:54 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -3,18 +3,19 @@
 #include “Driver/DrvSYS.h”  
 #include “Driver/DrvGPIO.h”
 #include “Driver/DrvUART.h”
-Void int()
+
+void int()
 {
 	DrvGPIO_Open(E_GPC,14,E_IO_OUTPUT);
 	DrvGPIO_SetBit(E_GPC,14);
 }
-Int main(void)
+int main(void)
 {
 	UNLOCKREG();        
 	DrvSYS_Open(48000000);
 	LOCKREG();
-Init()
-While (1)
+init()
+while (1)
 {	
 	DrvGPIO_ClrBit(E_GPC,15);
         DrvGPIO_Delay(300000)
