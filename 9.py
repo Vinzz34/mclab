b3eecfa943a5b35a41d8d669eff8820989b4ac0a
Vinay G N <76006184+Vinzz34@users.noreply.github.com>
2023-07-24 21:06:38 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,3 +1,14 @@
+"""
+To access remotely :
+Search PuTTY on Windows
+Open
+Enter the ip address of the Raspberry pi
+In Cmd
+Username : pi
+Password : raspberry
+locate file
+run using "Python3 file_name"
+"""
 import RPi.GPIO as GPIO
 import time
 # Set the GPIO mode to BCM
@@ -6,13 +17,13 @@ GPIO.setmode(GPIO.BCM)
 led_pin = 17
 GPIO.setup(led_pin, GPIO.OUT)
 try:
-while True:
-# Turn on the LED
-GPIO.output(led_pin, GPIO.HIGH)
-time.sleep(1) # Wait for 1 second
-# Turn off the LED
-GPIO.output(led_pin, GPIO.LOW)
-time.sleep(1) # Wait for 1 second
+  while True:
+    # Turn on the LED
+    GPIO.output(led_pin, GPIO.HIGH)
+    time.sleep(1) # Wait for 1 second
+    # Turn off the LED
+    GPIO.output(led_pin, GPIO.LOW)
+    time.sleep(1) # Wait for 1 second
 except KeyboardInterrupt:
 # Clean up GPIO configuration on keyboard interrupt (Ctrl+C)
 GPIO.cleanup() 
