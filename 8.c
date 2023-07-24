1b663954029f7d8a095a97bee7d5bfb4b9526439
Vinay G N <76006184+Vinzz34@users.noreply.github.com>
2023-07-24 22:52:48 +0530

    Update 8.c

--- 8.c
+++ 8.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 #include "NUC1xx.h"
-#include "Driver\DrvSYS.h"
 #include "Driver\DrvGPIO.h"
-#include "Driver\DrvADC.h"
+#include "Driver\DrvUART.h"
+#include "Driver\DrvSYS.h"
+
+
+void EINT1Callback(void) 
+{
+  DrvGPIO_ClrBit(E_GPB,11); 
+	DrvSYS_Delay(100000);	   
+	DrvGPIO_SetBit(E_GPB,11); 	
+	DrvSYS_Delay(100000);		
+}
 
 int main (void)
-{					   
-	UNLOCKREG();	
-  DrvSYS_Open(48000000);   
+{
+	UNLOCKREG();
+	DrvSYS_SetOscCtrl(E_SYS_XTL12M, 1); 
+	DrvSYS_Delay(5000);               
+	DrvSYS_SelectHCLKSource(0);     
 	LOCKREG();
 
-	DrvGPIO_Open(E_GPB, 11, E_IO_OUTPUT); 
+	DrvGPIO_Open(E_GPB, 11, E_IO_OUTPUT);
+   
+	//Interrupt (Don't change code here)
+  DrvGPIO_Open(E_GPB, 15, E_IO_INPUT);                             
+  DrvGPIO_EnableEINT1(E_IO_BOTH_EDGE, E_MODE_EDGE, EINT1Callback);
 
-	while(1) {
-  DrvGPIO_ClrBit(E_GPB,11); 
-	DrvSYS_Delay(100000);
-	DrvGPIO_SetBit(E_GPB,11); 
-	DrvSYS_Delay(100000);			
-	}
+  while(1)
+	{
+  }
 }
